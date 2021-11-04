ffe11ce542f7d4eea8b7450d142dd0422a29937d
Duck1s <32653@ma-web.nl>
2021-11-04 09:49:55 +0100

    Microbit opdracht veranderen

--- MicrobitDice.py
+++ MicrobitDice.py
@@ -1,6 +1,15 @@
 from microbit import *
-import random
+
+x_offset = 2
+y_offset = 2
 
 while True:
-    if accelerometer.was_gesture('shake'):
-        display.show(random.randint(1, 6))
+    x = int(accelerometer.get_x() / 250 + x_offset)
+    y = int(accelerometer.get_y() / 250 + x_offset)
+    if x > 4: x = 4
+    if x < 0: x = 0
+    if y > 4: y = 4
+    if y < 0: y = 0
+    display.set_pixel(x,y,5)
+    if button_a.is_pressed():
+        display.clear()
